4f83399d696fcae0cfd611162fe229f5febf573d
LTarazona <leandrotarazona@outlook.com>
2021-08-31 16:01:17 -0300

    Agregue alternativa para usar vectores

--- calibracion.c
+++ calibracion.c
@@ -3,12 +3,19 @@
 // offset[2] = offz
 #include "calibracion.h"
 
-signed int offset[3] = {0, 0, 0};
-float scale[3] = {1, 1, 1};
-signed int Min[3] = {0,0,0};
-signed int Max[3] = {0,0,0};
+/* Flags */
+unsigned char flags_acc;
+
+/* */
 
 
+/* Ver la posibilidadde grabar los valores de offset y escala en flash */
+signed  int offset[3] = {0, 0, 0};
+float       scale[3] = {1.0, 1.0, 1.0};
+signed  int Min[3] = {0,0,0};
+signed  int Max[3] = {0,0,0};
+signed  int Acc[3] = {0,0,0};
+/*
 void AccValoresCalibracion(signed int * Acc)
 {
     unsigned char i = 0;
@@ -22,8 +29,21 @@ void AccValoresCalibracion(signed int * Acc)
         {
             Max[i] = Acc[i];
         }
-    } 
+    }
+}*/
+
+void ValoresDeCalibracion(unsigned char eje)
+{
+    if (Acc[eje] < Min[eje])
+    {
+        Min[eje] = Acc[eje];
+    }
+    if (Acc[eje] > Max[eje])
+    {
+        Max[eje] = Acc[eje];
+    }
 }
+
 
 void AccCalcularOffset(void)
 {
@@ -32,13 +52,54 @@ void AccCalcularOffset(void)
     {
         offset[i] = (Max[i]+Min[i])/2;
     }
+        /* Grabar en flash */ 
 }
 
-void CalularEscala(void)
+void CalcularEscala(void)
 {
     unsigned char i = 0;
     for(i=0; i<3; i++)
     {
         scale[i] = GRAVEDAD/(Max[i]-offset[i]);
     }
+        /* Grabar en flash */ 
+}
+
+/* Tengo que hacer que se grabe un eje por vez cuando llegue a esa posicion */
+unsigned char SecuenciaCalibrado(void)
+{
+    static unsigned char estadoCalibracion = EST_CALIB_INICIO; 
+    unsigned char codEjeActivado = 0; // Codigo del eje que se esta disparando por la gravedad
+    unsigned char retval = 0;
+    
+    switch(estadoCalibracion)
+    {
+    case EST_CALIB_INICIO:
+        /* Paso a la espera por ahora */
+        flags_acc = 0;
+        estadoCalibracion = EST_CALIB_ESPERA;
+        break;
+    case EST_CALIB_ESPERA:
+        /* Codigo de espera para el siguiente paso en la calibracion */
+        codEjeActivado = ObtenerAccDisparada();
+        if(flags_acc == CALIBRACION_COMPLETA)
+        {
+            estadoCalibracion = EST_CALIB_INICIO;
+            retval = 1;
+            break;
+        }
+        else
+        if(codEjeActivado == INDETERMINADO)
+            break;
+        else
+            estadoCalibracion = EST_CALIB_EJECUTAR;
+/*omito el break para saltar al siguiente estado sin usar static en la variable codEjeActivado*/        
+    case EST_CALIB_EJECUTAR:
+        /* Ver segun que bit estoy recorriendo, segun el codigo que me mando antes */
+        break;
+        
+    default:
+        break;
+    } 
+
 }
--- calibracion.h
+++ calibracion.h
@@ -8,6 +8,26 @@
 
 #define GRAVEDAD    1
 
+#define FLAG_XP     ((flags_acc)&0x01)
+#define FLAG_YP     ((flags_acc>>1)&0x01)
+#define FLAG_ZP     ((flags_acc>>2)&0x01)
+#define FLAG_XN     ((flags_acc>>3)&0x01)
+#define FLAG_YN     ((flags_acc>>4)&0x01)
+#define FLAG_ZN     ((flags_acc>>5)&0x01)
+
+enum{
+    EST_CALIB_INICIO,
+    EST_CALIB_ESPERA,
+    EST_CALIB_XPOS,
+    EST_CALIB_XNEG,
+    EST_CALIB_YPOS,
+    EST_CALIB_YNEG,
+    EST_CALIB_ZPOS,
+    EST_CALIB_ZNEG,  
+    EST_CALIB_EJECUTAR,        
+};
+
 void AccValoresCalibracion(signed int * Acc);
 void AccCalcularOffset(void);
-void CalularEscala(void);
+void CalcularEscala(void);
+
